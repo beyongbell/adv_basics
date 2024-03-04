dfc8d73a7739c4f75561977dc4f5ec392bae02cc
beyongbell <tinnakornchoompee@gmail.com>
2024-03-04 15:35:35 +0700

    Set Layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'package:adv_basics/start_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
@@ -20,6 +25,6 @@ void main() {
           child: const StartScreen(),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
+          const Image(
+            image: AssetImage('assets/images/quiz-logo.png'),
             width: 300,
-            color: const Color.fromARGB(150, 255, 255, 255),
+            color: Color.fromARGB(150, 255, 255, 255),
           ),
+          // Image.asset(
+          //   'assets/images/quiz-logo.png',
+          //   width: 300,
+          //   color: const Color.fromARGB(150, 255, 255, 255),
+          // ),
           const SizedBox(
             height: 80,
           ),
